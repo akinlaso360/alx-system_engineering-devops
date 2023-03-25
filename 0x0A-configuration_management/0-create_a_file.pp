e8fa447568872d04e4d75c9d849cc7f7331487c2
akinlaso360 <akinlasonasirudeen@gmail.com>
2023-03-25 06:15:18 -0700

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Using Puppet, create a file
 file { '/tmp/school':
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
